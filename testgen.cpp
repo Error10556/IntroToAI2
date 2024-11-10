b10784989cd621e9bbddd15ca8bfd9042abfd67a
Timur <t.usmanov@innopolis.university>
2024-11-10 22:12:46 +0300

    Copy randgen into testgen

--- testgen.cpp
+++ testgen.cpp
@@ -1,16 +1,39 @@
 #include "sudokusolve.h"
+#include <cstdio>
 #include <iostream>
+#include <climits>
 using namespace std;
+
+FILE* randgen;
+
+int randint()
+{
+    int res;
+    fread(&res, sizeof(int), 1, randgen);
+    res &= INT_MAX;
+    return res;
+}
+
+int randint(int n)
+{
+    return randint() % n;
+}
+
+int randint(int min, int exmax)
+{
+    return randint(exmax - min) + min;
+}
 
 
 
 int main(int argc, char** argv)
 {
+    randgen = fopen("/dev/random", "r");
     if (argc == 1)
     {
         cout << "Usage: testgen directory #OfInitialDigits1 #OfTests1 "
                 "#OfInitDigits2 #OfTests2 ...\n";
         return 0;
     }
-    
+    fclose(randgen);
 }
