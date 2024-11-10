33105108d07456a7809786d1de20f9f0b76c4115
Timur <t.usmanov@innopolis.university>
2024-11-10 22:21:07 +0300

    Add randomness to solver

--- sudokusolve.cpp
+++ sudokusolve.cpp
@@ -12,7 +12,8 @@ void NextUnknown(const Sudoku& s, int& x, int& y)
     } while (y < 9 && s.Cell(y, x) != 0);
 }
 
-void DFS(Sudoku& cur, vector<Sudoku>& sols, int x, int y, int maxsols)
+void DFS(Sudoku& cur, vector<Sudoku>& sols, int x, int y, int maxsols,
+         ShuffleProc shuffler, void* shArgs)
 {
     if (y >= 9)
     {
@@ -23,12 +24,17 @@ void DFS(Sudoku& cur, vector<Sudoku>& sols, int x, int y, int maxsols)
     NextUnknown(cur, nx, ny);
     auto avail = cur.Available(y, x);
     auto cell = cur.Cell(y, x);
+    vector<int> digs;
     for (int i = 1; i <= 9; i++)
     {
-        if (!avail.test(i))
-            continue;
+        if (avail.test(i))
+            digs.push_back(i);
+    }
+    shuffler(digs, shArgs);
+    for (int i : digs)
+    {
         cell = i;
-        DFS(cur, sols, nx, ny, maxsols);
+        DFS(cur, sols, nx, ny, maxsols, shuffler, shArgs);
         if (sols.size() == maxsols)
             break;
     }
@@ -36,12 +42,18 @@ void DFS(Sudoku& cur, vector<Sudoku>& sols, int x, int y, int maxsols)
     return;
 }
 
-std::vector<Sudoku> SolveDFS(Sudoku s, int maxresults)
+std::vector<Sudoku> SolveDFS(const Sudoku& s, int maxresults)
+{
+    return SolveDFS(s, maxresults, [](vector<int>&, void*){}, nullptr);
+}
+
+std::vector<Sudoku> SolveDFS(Sudoku s, int maxresults, ShuffleProc shuffler,
+                             void* shuffleArgs)
 {
     int x, y;
     NextUnknown(s, x, y);
     vector<Sudoku> res;
-    DFS(s, res, x, y, maxresults);
+    DFS(s, res, x, y, maxresults, shuffler, shuffleArgs);
     return res;
 }
 
--- sudokusolve.h
+++ sudokusolve.h
@@ -1,6 +1,10 @@
 #pragma once
-#include <vector>
 #include "sudoku.h"
+#include <vector>
 
-std::vector<Sudoku> SolveDFS(Sudoku s, int maxresults);
+typedef void (*ShuffleProc)(std::vector<int>& digits, void* args);
+
+std::vector<Sudoku> SolveDFS(const Sudoku& s, int maxresults);
+std::vector<Sudoku> SolveDFS(Sudoku s, int maxresults, ShuffleProc shuffler,
+                             void* shufflerArgs);
 bool SolveDFS(Sudoku& s);
